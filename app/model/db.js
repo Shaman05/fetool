be5934250f0ff7b3ac5847f709f4df29433b0945
Chen Chao <chenchao@upg.cn>
2014-04-02 16:34:03 +0800

    add error message on open db error

--- app/model/db.js
+++ app/model/db.js
@@ -13,7 +13,10 @@ mongoose.connect(config.db.host + config.db.name);
 module.exports = {
   connect: function(callback){
     var db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error:'));
+    //db.on('error', console.error.bind(console, 'connection error:'));
+    db.on('error', function(){
+      callback({msg: '数据库连接失败'});
+    });
     db.once('open', function(){
       //已建立连接
       callback && callback();
