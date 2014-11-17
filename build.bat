92dd045fe88dd1d8310a25a33b4cc748f649bbb2
陈超 <chenchao@upg.cn>
2014-11-17 19:41:14 +0800

    .

--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@ tar cf fetool.tar app public package.json
 gzip fetool.tar
 mv fetool.tar.gz fetool.nw
 copy /b nw.exe+fetool.nw fetool.exe
-mv fetool.exe release/
-rm fetool.nw
+#mv fetool.exe release/
+#rm fetool.nw
